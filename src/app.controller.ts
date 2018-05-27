536bad76c8be4e6466425c68b72236339a6fb291
Ruben Soler <r.solerginer@gmail.com>
2018-05-27 17:52:39 +0200

    feat(query): simple query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Get, Controller } from '@nestjs/common';
+import { Get, Controller, Post, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Query } from './interface/query.model';
+import * as fetch from 'isomorphic-fetch';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   root(): string {
     return this.appService.root();
+  }
+
+  @Get('/query/:text')
+  query(@Param() params): Promise<any> {
+    return fetch(`http://localhost:5000/parse?q=${params.text}`)
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        return response.json();
+      });
   }
 }
